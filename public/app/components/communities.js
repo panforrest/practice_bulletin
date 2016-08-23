176480ced0b7c244b0c71fd3c13e24142f7475cb
panforrest <guoqianp@gmail.com>
2016-08-22 20:15:20 -0400

    practice_bulletin

---
+++ public/app/components/communities.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Communities extends Component {
+
+    render() {
+    	return(
+    		<div>
+    		    This is community Component!
+
+    		</div>
+
+    	)
+    }
+
+}
+
+export default Communities
